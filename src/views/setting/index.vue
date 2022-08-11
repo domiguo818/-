9caf722eb6077c3d17cdc33a715397faa08601b9
domiguo818 <1193224158@qq.com>
2022-08-11 09:08:24 +0800

    完成了公司设置的基本结构

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,7 +1,64 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>公司设置</h2>
+      <el-card>
+        <el-tabs>
+          <!-- 放置页签 -->
+          <el-tab-pane label="角色管理">
+            <!-- 新增角色按钮 -->
+            <el-row style="height: 60px">
+              <el-button icon="el-icon-plus" size="small" type="primary"
+                >新增角色</el-button
+              >
+            </el-row>
+            <!-- 表格 -->
+            <el-table :data="tableData" style="width: 100%">
+              <el-table-column prop="date" label="日期" width="180">
+              </el-table-column>
+              <el-table-column prop="name" label="姓名" width="180">
+              </el-table-column>
+              <el-table-column prop="address" label="地址"> </el-table-column>
+            </el-table>
+            <!-- 分页组件 -->
+            <el-row
+              type="flex"
+              justify="center"
+              align="middle"
+              style="height: 60px"
+            >
+              <!-- 分页组件 -->
+              <el-pagination layout="prev,pager,next" />
+            </el-row>
+          </el-tab-pane>
+          <el-tab-pane label="公司信息">
+            <el-alert
+              title="对公司名称、公司地址、营业执照、公司地区的更新，将使得公司资料被重新审核，请谨慎修改"
+              type="info"
+              show-icon
+              :closable="false"
+            />
+            <el-form label-width="120px" style="margin-top: 50px">
+              <el-form-item label="公司名称">
+                <el-input disabled style="width: 400px" />
+              </el-form-item>
+              <el-form-item label="公司地址">
+                <el-input disabled style="width: 400px" />
+              </el-form-item>
+              <el-form-item label="邮箱">
+                <el-input disabled style="width: 400px" />
+              </el-form-item>
+              <el-form-item label="备注">
+                <el-input
+                  type="textarea"
+                  :rows="3"
+                  disabled
+                  style="width: 400px"
+                />
+              </el-form-item>
+            </el-form>
+          </el-tab-pane>
+        </el-tabs>
+      </el-card>
     </div>
   </div>
 </template>
@@ -9,13 +66,36 @@
 <script>
 export default {
   data() {
-    return {}
+    return {
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
+    };
   },
 
   created() {},
 
-  methods: {}
-}
+  methods: {},
+};
 </script>
 
 <style scoped lang="less"></style>
